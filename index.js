fc243ad6b7ce5634193d0b903ccdeb6d7de4d326
kavitha-047 <kavithakanagraj470@gmail.com>
2025-01-15 10:30:00 +0530

    Initial commit: Add project structure and config

---
+++ index.js
@@ -0,0 +1,10 @@
+// Simple Hello World Application
+console.log("Welcome to git-contribut project!");
+console.log("Date: " + new Date());
+console.log("This is a contribution tracking system");
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("Developer"));
